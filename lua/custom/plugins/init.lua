3b8950c2cb4de54230483cd2cc6c3d572859781d
Damian Czaja <trojan295@protonmail.com>
2025-03-27 17:29:27 +0100

    add avante

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -118,6 +118,68 @@ return {
     'tenxsoydev/size-matters.nvim',
   },
   {
-    'github/copilot.vim',
+    'zbirenbaum/copilot.lua',
+    config = function()
+      require('copilot').setup {
+        --suggestion = {
+        --  enabled = true,
+        --  auto_trigger = true,
+        --},
+      }
+    end,
+  },
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    version = false, -- Never set this value to "*"! Never!
+    opts = {
+      provider = 'copilot',
+      auto_suggestions_provider = 'copilot',
+
+      behaviour = {
+        auto_suggestions = true,
+      },
+      mappings = {
+        suggestion = {
+          accept = '<Tab>',
+        },
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = 'make',
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
   },
 }
